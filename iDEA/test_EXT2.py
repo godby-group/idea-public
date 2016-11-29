86782190537dc169c386cd8fdedbcda1909f04c2
Leopold Talirz <leopold.talirz@gmail.com>
2016-11-29 12:06:31 +0000

    made EXT2-test faster and self-contained

--- iDEA/test_EXT2.py
+++ iDEA/test_EXT2.py
@@ -7,9 +7,6 @@ import EXT2
 import input
 import unittest
 
-# decimal places for comparison of results
-d = 10
-
 class TestHarmonicOscillator(unittest.TestCase):
     """ Tests for the harmonic oscillator potential
     
@@ -20,9 +17,22 @@ class TestHarmonicOscillator(unittest.TestCase):
     def setUp(self):
         """ Sets up harmonic oscillator system """
         pm = input.Input()
-        pm.ext.ctmax = 1e4  # larger steps
-        pm.ext.cdeltat = pm.ext.ctmax/(pm.ext.cimax-1)
+        pm.run.save = False
         pm.run.verbosity = 'low'
+
+        # It might still be possible to speed this up
+        pm.sys.NE = 2                  #: Number of electrons
+        pm.sys.grid = 61               #: Number of grid points (must be odd)
+        pm.sys.xmax = 7.5              #: Size of the system
+        pm.sys.acon = 1.0              #: Smoothing of the Coloumb interaction
+        pm.sys.interaction_strength = 1#: Scales the strength of the Coulomb interaction
+        def v_ext(x):
+            """Initial external potential"""
+            return 0.5*(0.25**2)*(x**2)
+        pm.sys.v_ext = v_ext
+        
+        pm.ext.ctol = 1e-5
+
         self.pm = pm
 
     def test_non_interacting_system_1(self):
@@ -31,12 +41,13 @@ class TestHarmonicOscillator(unittest.TestCase):
         pm.sys.interaction_strength = 0.0
         results = EXT2.main(pm)
 
-        nt.assert_allclose(results.gs_ext_E, 0.49988695, atol=1e-6)
+        #nt.assert_allclose(results.gs_ext_E, 0.49988695, atol=1e-6)
+        nt.assert_allclose(results.gs_ext_E, 0.500, atol=1e-3)
 
     def test_interacting_system_1(self):
         """Test interacting system"""
         pm = self.pm
         results = EXT2.main(pm)
 
-        nt.assert_allclose(results.gs_ext_E, 0.75310393, atol=1e-6)
+        nt.assert_allclose(results.gs_ext_E, 0.753, atol=1e-3)
 
