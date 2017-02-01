6c7037c4a7f73bbab1c6493cd00d0085bcccdf95
Leopold Talirz <leopold.talirz@gmail.com>
2017-02-01 16:16:39 +0000

    brought __init.py back

---
+++ iDEA/__init__.py
@@ -0,0 +1,24 @@
+"""interacting Dynamic Electrons Approach (iDEA)
+
+The iDEA code allows to propagate the time-dependent Schroedinger equation for
+2-3 electrons in one-dimensional real space.
+Compared to other models, such as the Anderson impurity model, this allows us
+to treat exchange and correlation throughout the system and provides additional
+flexibility in bridging the gap between model systems and ab initio
+descriptions.
+"""
+make_fortran = True
+
+if make_fortran:
+    # make Fortran libraries
+    import os
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    import subprocess
+    # note: this could be made more clever to
+    #   automatically detect different environments
+    #msg = subprocess.check_output(["make"], cwd=dir_path)
+    p = subprocess.Popen(["make"], cwd=dir_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if err:
+        print(err)
+        raise ImportError("Error while compiling Fortran libraries. Try typing 'make' in iDEA subdirectory.".format(err))
