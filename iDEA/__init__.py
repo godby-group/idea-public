745a16b26ffb09b989a4a24cf9c53fb1daa4feb7
Leopold Talirz <leopold.talirz@gmail.com>
2017-02-01 16:13:13 +0000

    automake fortran files
    now works properly
    checks whether fortran files have changed and recompiles if necessary

--- iDEA/__init__.py
+++
@@ -1,9 +0,0 @@
-"""interacting Dynamic Electrons Approach (iDEA)
-
-The iDEA code allows to propagate the time-dependent Schroedinger equation for
-2-3 electrons in one-dimensional real space.
-Compared to other models, such as the Anderson impurity model, this allows us
-to treat exchange and correlation throughout the system and provides additional
-flexibility in bridging the gap between model systems and ab initio
-descriptions.
-"""
