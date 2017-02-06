924662280a4d6875fead2f08480d76bc6152d04e
Leopold Talirz <leopold.talirz@gmail.com>
2017-02-06 18:15:13 +0000

    implemented pulay density mixer
    lda currently does potential mixing - that's why the
    Kerker preconditioner as implemented currently doesn't help.
    
    Next, I am going to move LDA.py over to density mixing

--- doc/intro/lda.rst
+++ doc/intro/lda.rst
@@ -1,2 +1,5 @@
 iDEA LDA
 ========
+
+.. [Kresse1996] 1.Kresse, G. & Furthmüller, J. Efficiency of ab-initio total energy calculations for metals and semiconductors using a plane-wave basis set. Computational Materials Science 6, 15–50 (1996). doi: 10.1016/0927-0256(96)00008-0
+
--- iDEA/LDA.py
+++ iDEA/LDA.py
@@ -113,6 +113,162 @@ def LHS(v,j):
          CNLHS[i,i-1] = -0.5j*pm.sys.deltat*(0.5/pm.sys.deltax**2)
    return CNLHS
 
+
+class PulayMixer:
+    """Performs Pulay mixing
+
+    Performs Pulay mixing with Kerker preconditioner,
+    as described on p.34 of [Kresse1996]_
+
+    """
+    def __init__(self, order, pm, q0=0.79):
+        """Initializes variables
+
+        parameters
+        ----------
+        order: int
+          order of Pulay mixing (how many densities to keep in memory)
+        pm: object
+          input parameters
+        q0: float
+          cutoff for Kerker mixing [1/a0]
+          Default corresponds to 1.5/Angstrom
+
+        """
+        self.order = order
+        self.step = 0
+        self.x_npt = pm.sys.grid
+        dtype = np.float
+        self.res = np.zeros((order,self.x_npt), dtype=dtype)
+        self.den_in = np.zeros((order,self.x_npt), dtype=dtype)
+
+        self.den_delta = np.zeros((order-1,self.x_npt), dtype=dtype)
+        self.res_delta = np.zeros((order-1,self.x_npt), dtype=dtype)
+        self.alpha_bar = np.zeros(order-1, dtype=dtype)
+
+
+        self.q0 = q0
+        self.A = 1.0
+        #self.G_q = np.zeros((x_npt,x_npt), dtype=np.float)
+        #for i in range(x_npt):
+        #    q = 2*np.pi * i/np.float(x_npt)
+        #    G[i,i] = A * q**2 / (q**2 + q0**2)
+        #self.G_q = np.zeros((x_npt), dtype=np.float)
+        #for i in range(x_npt):
+        #    q = 2*np.pi * i/np.float(x_npt)
+        self.G_q = np.zeros((self.x_npt/2+1), dtype=np.float)
+        dq = 2*np.pi / (2 * pm.sys.xmax)
+        q0_scaled = q0 / dq
+        print("scaled q {}".format(q0_scaled))
+        # eqn (82)
+        for i in range(len(self.G_q)):
+            q = 2*np.pi * i/np.float(self.x_npt)
+            self.G_q[i] = self.A * q**2 / (q**2 + q0_scaled**2)
+        #self.G_r = np.set_diagonal(diagonal
+
+
+
+    @property
+    def m(self):
+        r"""returns current array index m
+
+        This corresponds to the index m usedom [Kresse1996]_ on p. 33-34
+        self.mix calculates rho_in^{m+1}.
+        """
+        return self.step % self.order
+
+    def update_arrays(self, den_in, den_out):
+        r"""Updates densities and residuals
+
+        Note: self.step becomes larger than self.order,
+        this simply overwrites data that is no longer needed.
+        """
+        m = self.m
+
+        # eqn (88)
+        self.den_in[m] = den_in
+        if self.step > 0:
+            self.den_delta[m-1] = self.den_in[m] - self.den_in[m-1]
+
+        self.res[m] = den_out - den_in
+        if self.step > 0:
+            self.res_delta[m-1] = self.res[m] - self.res[m-1]
+
+    def compute_coefficients(self):
+        r"""Computes mixing coefficients
+        
+        Computes
+
+        See [Kresse1996]_ equations (87) - (90)
+
+        .. math ::
+
+            A_{ij} = \langle R[\rho^j_{in}] | R[\rho_{in}^i \rangle \\
+            \bar{A}_{ij} = \langle \Delta R^j | \Delta R^i \rangle
+        
+        """
+
+        # we return rhoin_m+1, which needs
+        # * delta_dens up to m-1
+        # * delta_rs up to m-1
+        # * alpha_bars up to m-1
+        # * rs up to m
+
+        # * delta_dens m-1 needs rho_in_m
+        # * delta_r m-1 needs r_m
+
+        # i.e. we need delta-quantities up to m-1
+        # and non-delta quantities up to m
+
+        m = self.m
+
+        # eqns (90,91)
+        overlaps = np.dot(self.res_delta, self.res[m])
+        A_bar = np.dot(self.res_delta, self.res_delta.T)
+        A_bar_inv = np.linalg.inv(A_bar)
+        alpha_bar = -np.dot(A_bar_inv.T,overlaps)
+
+        return alpha_bar
+
+    def mix(self, den_in, den_out):
+        r"""Compute mix of densities
+        
+        See [Kresse1996]_ equation (92)
+        """
+
+        self.update_arrays(den_in, den_out)
+
+        # until we have a sufficient history of densities, we don't mix
+        if self.step < self.order:
+            den_in_new = den_out
+        else:
+            alpha_bar = self.compute_coefficients()
+            print("")
+            print(alpha_bar)
+            m = self.m
+            # eqn (92)
+            den_in_new = den_in + self.precondition(self.res[m]) \
+                    + np.dot(self.alpha_bar, self.den_delta + self.precondition(self.res_delta))
+
+        self.step = self.step + 1
+
+        return den_in_new
+
+    def precondition(self, f):
+        """Return preconditioned f"""
+        #TODO: To implement
+        import matplotlib.pyplot as plt
+        x = np.linspace(-10,10,self.x_npt)
+        plt.plot(x,f)
+
+        f = np.fft.rfft(f, n=self.x_npt)
+        f = self.G_q * f
+        f = np.fft.irfft(f, n=self.x_npt)
+        plt.plot(x,f)
+        plt.show()
+        return f
+        
+
 # Main function
 def main(parameters):
    global pm, T
@@ -122,37 +278,48 @@ def main(parameters):
    T[0,:] = np.ones(pm.sys.grid)/pm.sys.deltax**2 # Define kinetic energy operator							
    T[1,:] = -0.5*np.ones(pm.sys.grid)/pm.sys.deltax**2 
 
-
+   if pm.lda.mix_type not in [None, 'linear', 'pulay']:
+       raise ValueError("lda.mix_type must be None, 'linear' or 'pulay'")
 
    v_s = np.zeros(pm.sys.grid,dtype='float')
    v_ext = np.zeros(pm.sys.grid,dtype='float')
-   Psi = np.zeros((pm.sys.NE,pm.sys.imax,pm.sys.grid), dtype='complex')
+   Psi = np.zeros((pm.sys.NE,pm.sys.imax,pm.sys.grid), dtype=np.complex)
    for i in xrange(pm.sys.grid):
       v_s[i] = pm.sys.v_ext((i*pm.sys.deltax-pm.sys.xmax)) # External potential
       v_ext[i] = pm.sys.v_ext((i*pm.sys.deltax-pm.sys.xmax)) # External potential
    n,waves,energies = groundstate(v_s) #Inital guess
-   U = Coulomb()
-   n_old = np.zeros(pm.sys.grid,dtype='float')
-   n_old[:] = n[:]
+   U = Coulomb() # Create Coulomb matrix
+   n_old = copy.deepcopy(n)
    convergence = 1.0
-   iteration = 1
-   while convergence>pm.lda.tol: # Use LDA
-      v_s_old = copy.copy(v_s)
-      if pm.lda.mix == 0:
-         v_s[:] = v_ext[:]+Hartree(n,U)+XC(n)
+   iteration = 0
+   #iteration = 1
+
+   if pm.lda.mix_type == 'pulay':
+       mixer = PulayMixer(order=5, pm = pm)
+
+   while convergence > pm.lda.tol and iteration < pm.lda.max_iter:
+      v_s_old = v_s
+      v_s_new = v_ext[:]+Hartree(n,U)+XC(n)
+
+      if pm.lda.mix_type == 'pulay':
+          v_s = mixer.mix(v_s_old, v_s_new)
+      elif pm.lda.mix_type == 'linear':
+          v_s[:] = (1-pm.lda.mix)*v_s_old[:]+pm.lda.mix*v_s_new[:]
       else:
-         v_s[:] = (1-pm.lda.mix)*v_s_old[:]+pm.lda.mix*(v_ext[:]+Hartree(n,U)+XC(n))
+          v_s = v_s_new
       n,waves,energies = groundstate(v_s) # Calculate LDA density 
       convergence = np.sum(abs(n-n_old))*pm.sys.deltax
       n_old[:] = n[:]
-      string = 'LDA: electron density convergence = ' + str(convergence)
+      string = 'LDA: electron density convergence = {:.4e}'.format(convergence)
       pm.sprint(string,1,newline=False)
       iteration += 1
-      if iteration > pm.lda.max_iter:
-         pm.sprint('LDA: Warning: Reached maximum number of iterations. Terminating',1)
+      if iteration == pm.lda.max_iter:
+         string = '\nLDA: reached maximum number of iterations {}. terminating self-consistency'.format(iteration)
+         pm.sprint(string,1)
          convergence = 0.0
 
    pm.sprint('',1)
+   pm.sprint('LDA: reached convergence in {} iterations.'.format(iteration),0)
    pm.sprint('LDA: ground-state xc energy: %s' % EXC(n),1)
    v_h = Hartree(n,U)
    v_xc = XC(n)
