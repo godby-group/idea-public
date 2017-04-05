9331713621d1cf4169a276525ed80aa17e89774b
Leopold Talirz <leopold.talirz@gmail.com>
2017-04-05 16:59:05 +0100

    added optimisation flag for Darwin

--- iDEA/arch/Darwin.mk
+++ iDEA/arch/Darwin.mk
@@ -3,5 +3,6 @@
 # . /opt/intel/mkl/bin/mklvars.sh intel64
 FC = gfortran
 FCFLAGS = -m64 -I$(MKLROOT)/include -Wtabs
+OPTFLAGS = -O3
 LFLAGS  = -L$(MKLROOT)/lib -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -liomp5 -ldl -lpthread -lm
 CLEAN = rm -rf *.so *.dSYM
