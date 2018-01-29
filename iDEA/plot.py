fe33a490a6fdbae90aa8ba0d40e178635fcdc8ac
Jack Wetherell <jw1294@york.ac.uk>
2018-01-29 15:50:00 +0000

    Fixed animation bug in ViDEO
    also added documentation to plot.py

--- iDEA/plot.py
+++ iDEA/plot.py
@@ -13,8 +13,17 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 ffmpeg_path = '/shared/storage/physrwg/trunk/iDEAL/packages/ffmpeg-3.4.1-64bit-static/ffmpeg'
 
 def read_quantity(pm, name):
-    """Reads quantity from pickle file
+    r"""Read a file from a pickle file in (/raw)
 
+    parameters
+    ----------
+    pm:  parameters object
+      parameters object
+    name: string
+      name of pickle file in (/raw) (e.g 'gs_ext_den')
+
+    returns array_like
+         data extracted from pickle file
     """
     input_file = open('raw/' + str(name) + '.db', 'rb')
     data = pickle.load(input_file)
@@ -22,8 +31,24 @@ def read_quantity(pm, name):
     return np.array(data)
 
 def to_data(pm, names, data, td, dim, file_name=None, timestep=0):
-    """Saves data to text file (/data)
+    r"""Outputs data to a .dat file in (/data)
 
+    parameters
+    ----------
+    pm:  parameters object
+      parameters object
+    names: list of string
+      names of the data to be saved (e.g 'gs_ext_den')
+    data: list of array_like
+      list of arrays to be plotted
+    td: bool
+      True: Time-dependent data, False: ground-state data
+    dim: int
+      number of dimentions of the data (0,1,2 or 3) (eg gs_ext_E would be 0, gs_ext_den would be 1)
+    file_name: string
+      name of output file (if None will be saved as default name e.g 'gs_ext_den.dat')
+    timestep: int
+      if td=True or data=3D specify the timestep to be saved
     """
     if len(data) > 1:
         raise IOError('cannot save multiple quantities to data file')
@@ -70,8 +95,24 @@ def to_data(pm, names, data, td, dim, file_name=None, timestep=0):
             raise IOError('cannot plot time-dependent 3D quantities to data file')
 
 def to_plot(pm, names, data, td, dim, file_name=None, timestep=0):
-    """Saves data to png (/png)
+    r"""Outputs data to a .pdf file in (/plots)
 
+    parameters
+    ----------
+    pm:  parameters object
+      parameters object
+    names: list of string
+      names of the data to be saved (e.g 'gs_ext_den')
+    data: list of array_like
+      list of arrays to be plotted
+    td: bool
+      True: Time-dependent data, False: ground-state data
+    dim: int
+      number of dimentions of the data (0,1,2 or 3) (eg gs_ext_E would be 0, gs_ext_den would be 1)
+    file_name: string
+      name of output file (if None will be saved as default name e.g 'gs_ext_den.pdf')
+    timestep: int
+      if td=True or data=3D specify the timestep to be saved
     """
     if type(data) is not list:
         data = [data]
@@ -225,8 +266,24 @@ def to_plot(pm, names, data, td, dim, file_name=None, timestep=0):
 
 
 def to_anim(pm, names, data, td, dim, file_name=None, step=1):
-    """Saves data to png (/png)
+    r"""Outputs data to a .mp4 file in (/animations)
 
+    parameters
+    ----------
+    pm:  parameters object
+      parameters object
+    names: list of string
+      names of the data to be saved (e.g 'gs_ext_den')
+    data: list of array_like
+      list of arrays to be plotted
+    td: bool
+      True: Time-dependent data, False: ground-state data
+    dim: int
+      number of dimentions of the data (0,1,2 or 3) (eg gs_ext_E would be 0, gs_ext_den would be 1)
+    file_name: string
+      name of output file (if None will be saved as default name e.g 'gs_ext_den.dat')
+    step: int
+      number of frames to skip when animating
     """
     if type(data) is not list:
         data = [data]
@@ -310,7 +367,7 @@ def to_anim(pm, names, data, td, dim, file_name=None, step=1):
             x = np.linspace(-pm.sys.xmax, pm.sys.xmax, pm.sys.grid)
             def animate(i):
                 for j in range(len(data)):
-                    line[j].set_data(x, data[j][i*int(step),:])
+                    line[j].set_data(x, data[j][i,:])
                     line[j].set_label(names[j])
                     ax.set_title('timestep = {}'.format(i))
                 legend = plt.legend()
@@ -319,7 +376,7 @@ def to_anim(pm, names, data, td, dim, file_name=None, step=1):
             print("making movie {}... (may take some time)".format(mfile))
             rc('animation', html='html5')
             plt.rcParams['animation.ffmpeg_path'] = ffmpeg_path
-            line_ani = animation.FuncAnimation(fig, animate, init_func=init, interval=10, blit=True)
+            line_ani = animation.FuncAnimation(fig, animate, frames=np.linspace(0, data.shape[2], int(step)), init_func=init, interval=10, blit=True)
             writer = animation.FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
             line_ani.save(mfile, writer=writer, dpi=300)
         if(dim == 2):
